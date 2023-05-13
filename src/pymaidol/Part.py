68352a5938bfb90b68bfc7e5d7665d89e3f88319
Eterance-PC <eterance@sina.com>
2023-05-13 15:48:23 +0800

    Part识别器：完成

--- src/pymaidol/Part.py
+++ src/pymaidol/Part.py
@@ -43,14 +43,16 @@ def PartRecognizor(template:str):
         current_part.end_line = current_line if current_line_index != 0 else current_line - 1
         current_part.end = current_line_index - 1 if current_line_index != 0 else last_line_final_index
         current_part.total_end = current_total_index - 1
-        list_of_parts.append(current_part)
+        if current_part.content != "":
+            list_of_parts.append(current_part)
         
     def _end_current_part_at_this_char():
         nonlocal current_part, current_line, current_line_index, current_total_index
         current_part.end_line = current_line 
         current_part.end = current_line_index
         current_part.total_end = current_total_index
-        list_of_parts.append(current_part)
+        if current_part.content != "":
+            list_of_parts.append(current_part)
     
     def _create_new_part_at_current_char():
         nonlocal current_part, current_line, current_line_index, current_total_index
@@ -74,53 +76,50 @@ def PartRecognizor(template:str):
         current_line += 1
         current_line_index = 0
         
-    def _embed_part_foresee_and_pair(consume_count:int, pair_left:str, pair_right:str):
+    def _foresee_and_pair(foresee_consume_count:int, pair_left:list[str], pair_right:list[str], is_include_right:bool=True):
         """_
 
         Args:
-            consume_count (int): 第一次要被消费的字符数。注意是包括@和前瞻数，比如 @if{，那么 consume_count = 4
+            consume_count (int): 第一次要被消费的字符数。注意是包括@和前瞻数，比如 @if{，那么 consume_count = 4; @，那么 consume_count = 1
             pair_left (str): _description_
             pair_right (str): _description_
         """
         nonlocal current_part, current_total_index, template
-        # 消费当前的 @ 和前瞻的词语
         _end_current_part_at_last_char()
         _create_new_part_at_current_char()
+        # 消费当前的 @ 和前瞻的词语
         # 一次吃多个字符
-        current_part.content = f"{current_part.content}{template[current_total_index:current_total_index+consume_count]}"
-        _consume_current_char_index(consume_count)
+        current_part.content = f"{current_part.content}{template[current_total_index:current_total_index+foresee_consume_count]}"
+        _consume_current_char_index(foresee_consume_count)
         # 持续消费，直到匹配到右侧
         pair_finder = PairFinder(pair_left, pair_right)
         while(current_total_index < len(template)):
-            current_part.content = f"{current_part.content}{template[current_total_index]}"
             pair_result = pair_finder.IsPair(template[current_total_index])
             if pair_result:
-                # 匹配到了右侧，结束当前 part
-                _end_current_part_at_this_char()
-                _consume_current_char_index()
-                _create_new_part_at_current_char()
+                if is_include_right:                    
+                    current_part.content = f"{current_part.content}{template[current_total_index]}"
+                    # 匹配到了右侧，结束当前 part
+                    _end_current_part_at_this_char()
+                    _consume_current_char_index()
+                    _create_new_part_at_current_char()
+                else:
+                    _end_current_part_at_last_char()
+                    _create_new_part_at_current_char()
                 break
+            current_part.content = f"{current_part.content}{template[current_total_index]}"
             _consume_current_char_index()
     
     while(current_total_index < len(template)):
         current_char = template[current_total_index]
         if current_char == '\n':
             current_part.content = f"{current_part.content}{current_char}"
+            _end_current_part_at_this_char()
             _consume_current_char_index_with_new_line()
+            _create_new_part_at_current_char()
             
         # 注释符号，一直匹配到 \n 为止
         elif current_char == '#':
-            _end_current_part_at_last_char()
-            _create_new_part_at_current_char()
-            current_part.content = f"{current_part.content}{current_char}"
-            _consume_current_char_index()
-            while(current_total_index < len(template)):
-                current_part.content = f"{current_part.content}{template[current_total_index]}"
-                if template[current_total_index] == '\n':
-                    _end_current_part_at_this_char()
-                    _consume_current_char_index_with_new_line()
-                    _create_new_part_at_current_char()
-                _consume_current_char_index()
+            _foresee_and_pair(1, [], ['\n'])
             
         # 可能的嵌入符号
         elif current_char == '@':
@@ -137,25 +136,24 @@ def PartRecognizor(template:str):
                 
             # 前瞻是 {，那么新建 part，匹配到 } 为止
             elif remaining.startswith('{'):
-                _embed_part_foresee_and_pair(2, '{', '}')
+                _foresee_and_pair(2, ['{'], ['}'])
             
             # 前瞻是 (，那么新建 part，匹配到 ) 为止
             elif remaining.startswith('('):
-                _embed_part_foresee_and_pair(2, '(', ')')
+                _foresee_and_pair(2, ['('], [')'])
             
-            elif remaining.startswith('if{'):
-                _embed_part_foresee_and_pair(4, '{', '}')
+            elif remaining.startswith('if('):
+                _foresee_and_pair(4, ['('], [')'])
                 
-            elif remaining.startswith('elif{'):
-                _embed_part_foresee_and_pair(6, '{', '}')
+            elif remaining.startswith('elif('):
+                _foresee_and_pair(6, ['('], [')'])
                 
-            elif remaining.startswith('for{'):
-                _embed_part_foresee_and_pair(5, '{', '}')
+            elif remaining.startswith('for('):
+                _foresee_and_pair(5, ['('], [')'])
                 
-            # 什么都不是，那么就是普通的 @，作为普通字符处理
+            # 一直消费字符直至遇到空白符（空白符不包括在内）
             else:
-                current_part.content = f"{current_part.content}{current_char}"
-                _consume_current_char_index()
+                _foresee_and_pair(1, [], [' ', '\t', '\n', '\r'], False)
             
         # 纯文本
         else:
@@ -167,16 +165,16 @@ def PartRecognizor(template:str):
     return list_of_parts
         
 class PairFinder():
-    def __init__(self, left:str, right:str) -> None:
+    def __init__(self, left:list[str], right:list[str]) -> None:
         self.__unpaired_left_count = 0
         self.__left = left
         self.__right = right
     
     def IsPair(self, target:str):
-        if target == self.__left:
+        if target in self.__left:
             self.__unpaired_left_count += 1
             return False
-        elif target == self.__right:
+        elif target in self.__right:
             if self.__unpaired_left_count == 0:
                 return True
             else:
--- src/tests/harder_demo/CodeLangTemplate.pml
+++ src/tests/harder_demo/CodeLangTemplate.pml
@@ -2,16 +2,21 @@
 
 @{count=1}
 @{dict2 = {1:"1212", 2:"3asx"}}
-@for{index, item in self.incontext_samples}
+@for(index, item in self.incontext_samples)
 -- Question @(index+1) # 这是注释
 # 这也是注释
 # 这还是注释
 
 Question: Write a @(item["lang"]) program that prints "Hello World!" to the console.
-Code: @(item["code"])
-
+Code: @(item["code"]) (@@(item["code"]))
+目前的count： @(count) \
+    @if(count >= 2)
+> 2!
+    @elif(count < 0)
+< 0???
+    @end     
 @{count+=1}
-@{end}
+@end
 -- Question @(item["count"])
 
 Question: Write a @(query_samples['lang']) program that prints "Hello World!" to the console.
