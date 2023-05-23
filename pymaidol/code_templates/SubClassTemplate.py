ca5cc1ef1d4814ec840e943258974048a5f02525
eterance <eterance@sina.com>
2023-05-23 10:56:35 +0000

    调整import

--- pymaidol/code_templates/SubClassTemplate.py
+++ pymaidol/code_templates/SubClassTemplate.py
@@ -1,4 +1,4 @@
-from TemplateBase import TemplateBase
+from pymaidol.TemplateBase import TemplateBase
 
 class SubClassTemplate(TemplateBase):
     def __init__(self, class_name:str, template: str | None = None, template_file_path: str | None = None) -> None:
